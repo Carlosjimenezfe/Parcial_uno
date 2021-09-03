8c28a79c3a1c9b390dea23470e2f45f0cffe5255
Carlos Jimenez <valuu1216@gmail.com>
2021-09-02 23:56:54 -0500

    Calcular la masa

--- Parcial_uno.py
+++ Parcial_uno.py
@@ -5,3 +5,8 @@ Created on Thu Sep  2 23:25:58 2021
 @author: Usuario
 """
 
+p= float(input('digite el valor de la presion:'))
+v=float(input('digite el valor del volumen:'))
+t=float(input('digite el valor de la temperatura:'))
+m=(p*v)/(0.37*(t+460))
+print(f"El valor de masa es: {m}")
