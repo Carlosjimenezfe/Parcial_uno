f48358515f8c9f0fca15d05ad8bdfbef753bfc5b
Carlos Jimenez <valuu1216@gmail.com>
2021-09-03 00:38:53 -0500

    Numero de pulsaciones

--- Parcial_uno.py
+++ Parcial_uno.py
@@ -5,8 +5,21 @@ Created on Thu Sep  2 23:25:58 2021
 @author: Usuario
 """
 
+"""
+Valor de la masa
+"""
+"""
 p= float(input('digite el valor de la presion:'))
 v=float(input('digite el valor del volumen:'))
 t=float(input('digite el valor de la temperatura:'))
 m=(p*v)/(0.37*(t+460))
 print(f"El valor de masa es: {m}")
+"""
+
+"""
+numero de pulsaciones
+"""
+
+e=int(input('digite la edad:'))
+p=(200-e)/10
+print('El numero de puslaciones son', p)
